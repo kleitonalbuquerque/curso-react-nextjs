1dfa552609899a637f5a4bf81eaac330f54ff4ad
kleitonalbuquerque <kleiton2102@gmail.com>
2022-12-28 16:03:49 -0300

    State com arrays e objetos

--- src/App.js
+++ src/App.js
@@ -1,76 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import { Component } from 'react';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
+import "./App.css";
+import { Component } from "react";
 class App extends Component {
-  constructor(props) {
-    super();
-    this.handlePClick = this.handlePClick.bind(this)
-
-    this.state = {
-      name: 'Kleiton Albuquerque',
-      counter: 0
-    };
-  }
-
-  handlePClick() {
-    // const { name } = this.state
-    // console.log(`<p>clicado</p> ${name}`)
-    this.setState({ name: 'Mariana Oliveira de Albuquerque' })
-  }
-
-  // função arrow substitui o bind
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  }
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "O título 1",
+        body: "O corpo 1",
+      },
+      {
+        id: 2,
+        title: "O título 2",
+        body: "O corpo 2",
+      },
+      {
+        id: 3,
+        title: "O título 3",
+        body: "O corpo 3",
+      },
+    ],
+  };
 
   render() {
-    const { name, counter } = this.state
+    const { posts } = this.state;
+
     return (
       <div className="App">
-       <header className="App-header">
-         <img src={logo} className="App-logo" alt="logo" />
-         {/* <p>
-           Edit <code>src/App.js</code> and save to reload.
-         </p> */}
-         <p onClick={ this.handlePClick }>
-          Nome: {name} {counter}
-         </p>
-         <a
-           onClick={ this.handleAClick }
-           className="App-link"
-           href="https:reactjs.org"
-           target="_blank"
-           rel="noopener noreferrer"
-         >
-           Learn React
-         </a>
-       </header>
-     </div>
-    )
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
