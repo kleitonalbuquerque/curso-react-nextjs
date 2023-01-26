174d4ba1bc54e3ac9b9cd5eb5fe365c7d69598ea
kleitonalbuquerque <kleiton2102@gmail.com>
2023-01-26 17:36:40 -0300

    Recovery

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global-styles.css';
 
-import { Home } from './templates/Home';
+import { Home } from './templates/Home/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home numberToIncrement={2} />
+    <Home />
   </React.StrictMode>
 );
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,29 +1,91 @@
-import { Component } from "react";
 import "./styles.css";
 
+import { Component } from "react";
+import { loadPosts } from "../../utils/load-posts";
+import { Posts } from "../../components/Posts";
+import { Button } from "../../components/Button";
+import { TextInput } from "../../components/TextInput";
 export class Home extends Component {
   state = {
-    counter: 0,
+    posts: [],
+    allPosts: [],
+    page: 0,
+    postsPerPage: 10,
+    searchValue: "",
   };
 
-  handleClick = () => {
-    this.setState(
-      (prevState, prevProps) => {
-        console.log("PREV", prevState.counter);
-        return { counter: prevState.counter + prevProps.numberToIncrement };
-      },
-      () => {
-        console.log("POST", this.state.counter);
-      }
-    );
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const { page, postsPerPage } = this.state;
+    const postsAndPhotos = await loadPosts();
+    this.setState({
+      posts: postsAndPhotos.slice(page, postsPerPage),
+      allPosts: postsAndPhotos,
+    });
+  };
+
+  loadMorePosts = async () => {
+    const { page, postsPerPage, allPosts, posts } = this.state;
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts);
+
+    this.setState({ posts, page: nextPage });
+  };
+
+  handleChange = (e) => {
+    // console.log(e.target.value)
+    const { value } = e.target;
+    this.setState({ searchValue: value });
   };
 
   render() {
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
+    const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue
+      ? allPosts.filter((post) => {
+          return post.title
+            .toLowerCase()
+            .includes(searchValue.toLocaleLowerCase());
+        })
+      : posts;
+
     return (
-      <div className="container">
-        <h1>{this.state.counter}</h1>
-        <button onClick={this.handleClick}>Increment</button>
-      </div>
+      <section className="container">
+        {/* Se houver busca exibe o título com o valor da busca */}
+        <div className="search-container">
+          {!!searchValue && (
+            <>
+              <h1>Search value: {searchValue}</h1>
+            </>
+          )}
+
+          <TextInput
+            searchValue={searchValue}
+            handleChange={this.handleChange}
+          />
+        </div>
+
+        {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+        {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
+
+        <div className="button-container">
+          {/* Se não houver busca exibe o botão */}
+          {!searchValue && (
+            <>
+              <Button
+                text="Load more posts"
+                onClick={this.loadMorePosts}
+                disabled={noMorePosts}
+              />
+            </>
+          )}
+        </div>
+      </section>
     );
   }
 }
